c32bd5014ab1e025b64416efddc8f66f7b6cc7a5
Daniil Shmoylov <dshmoylov@icerockdev.com>
2025-05-16 00:32:49 +0300

    Initial Commit

---
+++ exporter-tcc/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  exporter-tcc
+//
+//  Created by Daniil Shmoylov on 16.05.2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
